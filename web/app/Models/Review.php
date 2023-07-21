cae05c93325485f6cf3669b139bfb63a904875d6
ryuwa <ryuusuzuki243@gmail.com>
2023-07-21 16:22:01 +0900

    add: reviewモデル作成

---
+++ web/app/Models/Review.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $table = 'reviews';
+    protected $fillable = [
+        'name',
+        'shop_id',
+        'product_id',
+        'rating',
+        'comment'
+    ];
+
+    protected $dates = [
+        'updated_at',
+        'created_at'
+    ];
+}
