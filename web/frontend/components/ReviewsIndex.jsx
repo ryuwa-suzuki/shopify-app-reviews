b16f5e9256e53f317f9bbbe0bc043af35adeb3e2
ryuwa <ryuusuzuki243@gmail.com>
2023-08-05 14:33:58 +0900

    update: review一覧画面にデータを反映

--- web/frontend/components/ReviewsIndex.jsx
+++ web/frontend/components/ReviewsIndex.jsx
@@ -1,62 +1,67 @@
 import {
+  Icon,
   IndexTable,
   LegacyCard,
-  useIndexResourceState,
-  Text,
-  Badge,
+  Thumbnail
 } from '@shopify/polaris';
 import React from 'react';
+import dayjs from "dayjs";
+import { ImageMajor } from "@shopify/polaris-icons";
 
-export function ReviewsIndex () {
-  const reviews = [
-    {
-      id: 1,
-      raiting: '☆☆☆☆☆',
-      date: 'Jul 20 at 4:34pm',
-      review: 'テストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキスト',
-      product: '半袖シャツ',
-      product_id: 1
-    },
-    {
-      id: 2,
-      raiting: '☆☆☆☆☆',
-      date: 'Jul 20 at 4:34pm',
-      review: 'テスト',
-      product: '半袖シャツ',
-      product_id: 1
-    }
-  ];
+export function ReviewsIndex ({reviews, loading}) {
   const resourceName = {
     singular: 'reviews',
     plural: 'reviews',
   };
 
-  const style = {
-    whiteSpace: 'wrap'
-  }
-
   const rowMarkup = reviews.map(
     (
-      {id, raiting, review, date, product},
+      {id, rating, comment, created_at, product, name},
       index,
-    ) => (
-      <IndexTable.Row
-        id={id}
-        key={id}
-        // selected={selectedResources.includes(id)}
-        position={index}
-      >
-        <IndexTable.Cell>{raiting}</IndexTable.Cell>
-        <IndexTable.Cell>{product}</IndexTable.Cell>
-        <IndexTable.Cell>
-          <div
-          style={style}>
-            {review}
-          </div>
-        </IndexTable.Cell>
-        <IndexTable.Cell>{date}</IndexTable.Cell>
-      </IndexTable.Row>
-    ),
+    ) => {
+
+      let stars = '';
+      for(let i = 0; i < 5; i++) {
+        if (i < rating) {
+          stars += '★';
+        } else {
+          stars += '☆';
+        }
+      }
+
+      return (
+        <IndexTable.Row
+          id={id}
+          key={id}
+          position={index}
+        >
+          <IndexTable.Cell>
+            {stars}
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            <Thumbnail
+              source={product?.image?.src || ImageMajor}
+              alt="placeholder"
+              color="base"
+              size="small"
+            />
+            {product?.handle || 'Delete Product'}
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            {name}
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            <div
+            style={{
+              whiteSpace: 'wrap'
+              }}>
+              {comment}
+            </div>
+          </IndexTable.Cell>
+          <IndexTable.Cell>{dayjs(created_at).format("MMMM D, YYYY")}</IndexTable.Cell>
+        </IndexTable.Row>
+      );
+    },
   );
 
   return (
@@ -68,9 +73,11 @@ export function ReviewsIndex () {
         headings={[
           {title: 'Raiting'},
           {title: 'Product'},
+          {title: 'Name'},
           {title: 'Review'},
           {title: 'Date'},
         ]}
+        loading={loading}
       >
         {rowMarkup}
       </IndexTable>
